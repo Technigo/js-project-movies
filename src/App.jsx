06191f081a5b8df84328c19abe07806253b3f960
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2025-05-13 14:44:39 +0200

    set up App.jsx basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages/Home'
+import MovieInfo from './pages/MovieInfo'
+
 export const App = () => {
   return (
+    <>
     <h1>Movies</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movieinfo" element={<MovieInfo />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
