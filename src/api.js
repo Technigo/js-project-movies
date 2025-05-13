637919e745d048557cb4b208dc37b70da2c0a818
Talo Vargas <govargas@gmail.com>
2025-05-13 20:03:06 +0200

    add .env and .env.example for TMDB API key management

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-export const API_KEY = '3874d076eeecb44dd8a6803095929816'
+export const API_KEY = import.meta.env.VITE_API_KEY
 export const BASE_URL = 'https://api.themoviedb.org/3'
 export const IMAGE_BASE = 'https://image.tmdb.org/t/p'
