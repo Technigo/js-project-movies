7f3981fa64a1d01cf02b49a237c6c75f66539651
llindallarsson <larsson.ll@hotmail.com>
2025-05-13 13:43:09 +0200

    background poster fit and movie cover fit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,22 +1,25 @@
 import styled from "styled-components";
-import { movieDetails } from '../hooks/fetch.js';
-import { useParams } from 'react-router-dom';
-
+import { movieDetails } from "../hooks/fetch.js";
+import { useParams } from "react-router-dom";
 
 const MovieBackground = styled.div`
-  background-image: url(https://image.tmdb.org/t/p/original${props => props.poster});
-  border: 2px solid black;
-  height: 100vh;
-  width: auto;
+  background-image: linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%),
+    url(https://image.tmdb.org/t/p/original${(props) => props.poster});
+  min-height: 100vh;
+  display: flex;
+  background-size: cover;
+  flex-direction: column;
+  justify-content: flex-end;
 `;
 
-const MoviePoster = styled.div`
-  background-image: url(https://image.tmdb.org/t/p/original${props => props.poster});
+const MovieInfo = styled.div``;
+
+const MoviePoster = styled.img`
   width: 200px;
-  height: 200px;
-  border: 5px solid green;
-  position: bottom;
-  `
+  height: 295px;
+  border: 5px solid white;
+  object-fit: cover;
+`;
 
 export const MovieDetails = () => {
   const { id } = useParams();
@@ -29,14 +32,14 @@ export const MovieDetails = () => {
   return (
     <div>
       <MovieBackground poster={details.poster_path}>
-      <h2>{details.title}</h2>
-      <p>{details.overview}</p>
-      <p>Release Date: {details.release_date}</p>
-      <p>Rating: {details.vote_average}/10</p>
-      <MoviePoster poster={details.poster_path}> bödfajbaködjbfkd</MoviePoster>
+        <MoviePoster
+          src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+          alt='Movie Poster'
+        />
+        <h2>{details.title}</h2>
+        <p>{details.overview}</p>
+        <p>Rating: {details.vote_average}/10</p>
       </MovieBackground>
     </div>
   );
 };
-
-
