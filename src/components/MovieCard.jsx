4e000f6099dea0aaf76f9913b3cd9da76fcd96d2
Talo Vargas <govargas@gmail.com>
2025-05-13 18:16:36 +0200

    create a MovieCard.jsx component to display a single movie card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,7 @@
+import { IMAGE_BASE } from '../api'
+// This component will display a single movie card
+const MovieCard = ({ id, title, release_date, vote_average, poster_path }) => {
+  return (
+}
+
+export default MovieCard
