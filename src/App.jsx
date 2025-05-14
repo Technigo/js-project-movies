e3fb3245ca0337b343851650646ea72c7acbb845
violacathrine <cathrineohlsson@live.se>
2025-05-14 20:54:17 +0200

    solved movieinfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header.jsx'
-import { Home } from './pages/Home.jsx'
-import { MovieInfo } from './pages/MovieInfo.jsx'
-import { NotFound } from './pages/NotFound.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header.jsx";
+import { Home } from "./pages/Home.jsx";
+import { MovieInfo } from "./pages/MovieInfo.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
 
 export const App = () => {
   return (
@@ -10,11 +10,11 @@ export const App = () => {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/movies/:title' element={<MovieInfo />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<MovieInfo />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
-import { useEffect, useState } from 'react'
-import { api } from '../api/api'
-import { Card } from '../components/Card.jsx'
-import { Link } from 'react-router-dom'
-import { device } from '../styles/media.js'
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { api } from "../api/api";
+import { Card } from "../components/Card.jsx";
+import { Link } from "react-router-dom";
+import { device } from "../styles/media.js";
 
 export const StyledHome = styled.section`
   width: 100%;
@@ -26,42 +26,35 @@ export const StyledHome = styled.section`
       justify-content: flex-start;
     }
   }
-`
+`;
 
 export const Home = () => {
-  const [movies, setMovies] = useState([])
-  const [error, setError] = useState(null)
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     api
       .fetchHorrorMovies()
       .then((data) => {
-        setMovies(data.results)
+        setMovies(data.results);
       })
       .catch((err) => {
-        console.error(err)
-        setError('Something went wrong when fetching movies.')
-      })
-  }, [])
+        console.error(err);
+        setError("Something went wrong when fetching movies.");
+      });
+  }, []);
 
   if (error) {
-    return <p>{error}</p>
+    return <p>{error}</p>;
   }
 
   return (
     <StyledHome>
-      <div className='movies'>
+      <div className="movies">
         {movies.map((movie) => (
-          <Card key={movie.id} movie={movie}>
-            <h2>{movie.title}</h2>
-            <p>Releasedate: {movie.release_date}</p>
-            <img
-              src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-              alt={`Poster for ${movie.title}`}
-            />
-          </Card>
+          <Card key={movie.id} movie={movie} />
         ))}
       </div>
     </StyledHome>
-  )
-}
+  );
+};
