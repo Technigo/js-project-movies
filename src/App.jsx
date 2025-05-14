1fcceeb25bd3fd49dadeaecfe3c695a8f92cc2d8
alex91-html <alex91-html@users.noreply.github.com>
2025-05-14 20:28:08 +0200

    BrowserRoute, Routes, Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRoute, Routes, Router } from "react-router"
+
+
 export const App = () => {
   return (
     <>
-      <h1>Movies</h1>
-    </>
-  )
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+
+
+
+        <h1>Movies</h1>
+      </>
+      )
 }
