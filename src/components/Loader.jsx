814c0f2306470cffb2e918a0d1cbbe30ed5d5287
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-05-14 21:16:25 +0200

    added Loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import { DotLottieReact } from '@dotlottie/react-player'
+
+export const Loader = () => {
+  return (
+    <DotLottieReact
+      src='https://lottie.host/97db6f68-80f0-44c4-b0e3-775fef1c99a1/sEbu2v3MDk.lottie'
+      loop
+      autoplay
+    />
+  )
+}
