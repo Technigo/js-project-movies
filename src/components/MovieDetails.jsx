f594fc70bea61d628a5fb206fd2b118022b3a99f
llindallarsson <larsson.ll@hotmail.com>
2025-05-14 09:09:27 +0200

    Styling for responsivity and rounded rating to one decimal

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import { movieDetails } from "../hooks/fetch.js";
 import { useParams } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
+import { IoCaretBackCircle } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-const MovieBackground = styled.div`
+const PosterBackground = styled.div`
   background-image: linear-gradient(rgba(0, 0, 0, 0) 70%, rgb(0, 0, 0) 100%),
     url(https://image.tmdb.org/t/p/original${(props) => props.poster});
   min-height: 100vh;
@@ -10,15 +13,76 @@ const MovieBackground = styled.div`
   background-size: cover;
   flex-direction: column;
   justify-content: flex-end;
+  color: white;
 `;
 
-const MovieInfo = styled.div``;
+const BackLink = styled(Link)`
+  position: absolute;
+  top: 16px;
+  left: 50px;
+  text-decoration: none;
+  color: white;
+  display: inline-flex;
+  align-items: center;
+  font-weight: 700;
+  filter: drop-shadow(2px 3px 4px rgb(0 0 0 / 0.5));
+`;
+
+const MovieInfoWrapper = styled.div`
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 577px) {
+    flex-direction: row;
+    align-items: flex-end;
+  }
+`;
+
+const MovieDetailsWrapper = styled.div`
+  @media (min-width: 577px) {
+    margin: 0 0 0 20px;
+  }
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 24px;
+  margin: 20px 0 0 0;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Title = styled.span`
+  margin: 0 10px 0 0;
+  text-shadow: 1px 1px #4d4d4d;
+`;
+const Rating = styled.span`
+  background-color: white;
+  color: black;
+  padding: 0 5px;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+`;
 
 const MoviePoster = styled.img`
-  width: 200px;
-  height: 295px;
+  width: 190px;
+  height: 285px;
   border: 5px solid white;
   object-fit: cover;
+
+  @media (min-width: 577px) {
+    width: 240px;
+    height: 360px;
+  }
+
+  @media (min-width: 770px) {
+    width: 352px;
+    height: 523px;
+  }
 `;
 
 export const MovieDetails = () => {
@@ -31,15 +95,42 @@ export const MovieDetails = () => {
 
   return (
     <div>
-      <MovieBackground poster={details.poster_path}>
-        <MoviePoster
-          src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
-          alt='Movie Poster'
-        />
-        <h2>{details.title}</h2>
-        <p>{details.overview}</p>
-        <p>Rating: {details.vote_average}/10</p>
-      </MovieBackground>
+      <PosterBackground poster={details.backdrop_path}>
+        <BackLink to='/'>
+          <IoCaretBackCircle
+            style={{
+              color: "white",
+              marginRight: "4px",
+              height: "30px",
+              width: "30px",
+            }}
+          />
+          Movies
+        </BackLink>
+        <MovieInfoWrapper>
+          <MoviePoster
+            src={`https://image.tmdb.org/t/p/w500${details.poster_path}`}
+            alt='Movie Poster'
+          />
+          <MovieDetailsWrapper>
+            <StyledH1>
+              <Title>{details.title}</Title>
+              <Rating>
+                <FaStar
+                  style={{
+                    color: "gold",
+                    marginRight: "4px",
+                    height: "18px",
+                    width: "18px",
+                  }}
+                />
+                {Number(details.vote_average).toFixed(1)}
+              </Rating>
+            </StyledH1>
+            <p>{details.overview}</p>
+          </MovieDetailsWrapper>
+        </MovieInfoWrapper>
+      </PosterBackground>
     </div>
   );
 };
