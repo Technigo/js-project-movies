2da815ddcdfa71315962989c9867aaa3984d2a8c
oskarnordin <oskarnordin1@gmail.com>
2025-05-15 13:37:08 +0200

    Update
    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { PopularMovies } from "./pages/PopularMovies"
 import { NewMovies } from "./pages/NewMovies"
@@ -19,23 +18,3 @@ export const App = () => {
     </main >
   )
 }
-=======
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PopularMovies } from "./pages/PopularMovies";
-import { NewMovies } from "./pages/NewMovies";
-import { Details } from "./pages/Details";
-// import { MoviePoster } from "./components/MoviePoster"
-
-export const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PopularMovies />} />
-        <Route path="/news" element={<NewMovies />} />
-        <Route path="/movie/:id" element={<Details />} />
-        {/* <Route path="/poster" element={<MoviePoster />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
-};
->>>>>>> Stashed changes
--- src/pages/PopularMovies.jsx
+++ src/pages/PopularMovies.jsx
@@ -1,60 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
-import styled from "styled-components";
-
-const PosterSection = styled.section`
-  box-sizing: border-box;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-auto-rows: 150px;
-  gap: 0px;
-  height: 100vh;
- 
-
-  @media (min-width: 600px) {
-    grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
-    grid-auto-rows: 200px;
-  }
-
-  @media (min-width: 1024px) {
-    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-    grid-auto-rows: 250px;
-  }
-`;
-
-const MoviePoster = styled.article`
-  border: 1px solid red;
-  padding: 10px;
-  background: #fafafa;
-  display: flex;
-  flex-direction: column;
-
-  /* Random span values passed as props */
-  grid-column: span ${props => props.colSpan};
-  grid-row: span ${props => props.rowSpan};
-`;
-
-
-const StyledLink = styled(Link)`
-  font-weight: bold;
-  font-size: 1rem;
-  color: #0055aa;
-  text-decoration: none;
-  margin-bottom: 0.5rem;
-
-  &:hover {
-    text-decoration: underline;
-    color: #003377;
-  }
-`;
-
-function randomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-=======
-// import { MoviePoster } from "../components/MoviePoster";
->>>>>>> Stashed changes
+import { MoviePoster } from "../components/MoviePoster";
 
 export const PopularMovies = () => {
   const [movies, setMovies] = useState([]);
