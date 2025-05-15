eaca7bb3b4fa16260b0b85abf2ada07457e9bc11
alex91-html <alex91-html@users.noreply.github.com>
2025-05-15 11:11:50 +0200

    fetching data api

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Movies = () => {
+  // here we can use the useEffect hook to fetch data from the API
+
+
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
+      .then((response) => response.json())
+      .then((data) => console.log(data.results))
+      .catch((error) => console.error('Error fetching Movies:', error));
+
+
+  }, []);
+
+
+
+
   return (
     <div>Movies </div>
   )
 }
 
 export default Movies
+
