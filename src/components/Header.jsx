14fab35a43c0275ad348d20b8bd2faf688650a91
Linda Schönfeldt <linda.schonfeldt@gmail.com>
2025-05-16 13:38:23 +0200

    minor fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
-import styled from "styled-components";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
+import styled from 'styled-components'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const StyledHeader = styled.header`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   background-color: ${(props) =>
-    props.$isTransparent ? "transparent" : "black"};
+    props.$isTransparent ? 'transparent' : 'black'};
   color: white;
   padding: 1rem;
   position: fixed;
@@ -15,12 +15,12 @@ const StyledHeader = styled.header`
   left: 0;
   width: 100%;
   z-index: 10;
-`;
+`
 
 const Title = styled.h1`
   margin: 0;
   font-size: 1.5rem;
-`;
+`
 
 const Nav = styled.nav`
   margin-top: 0.5rem;
@@ -32,7 +32,7 @@ const Nav = styled.nav`
     font-weight: bold;
     font-size: 1.2rem;
   }
-`;
+`
 
 const BackButton = styled.button`
   background: none;
@@ -42,37 +42,34 @@ const BackButton = styled.button`
   cursor: pointer;
   margin-top: 1rem;
   padding: 0;
-`;
+`
 
 export const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate(); // Nu är navigate definierad
-  console.log("useNavigate:", navigate); // Kontrollera om navigate är en funktion
+  const location = useLocation()
+  const navigate = useNavigate() // Nu är navigate definierad
+  console.log('useNavigate:', navigate) // Kontrollera om navigate är en funktion
 
-  const [isTransparent, setIsTransparent] = useState(false);
+  const [isTransparent, setIsTransparent] = useState(false)
 
   useEffect(() => {
-    const isMovieInfoPage = location.pathname.startsWith("/movies/");
-    setIsTransparent(isMovieInfoPage);
-  }, [location]);
+    const isMovieInfoPage = location.pathname.startsWith('/movies/')
+    setIsTransparent(isMovieInfoPage)
+  }, [location])
 
   const handleGoBack = () => {
-    navigate("/");
-  };
+    navigate('/')
+  }
 
   return (
     <StyledHeader $isTransparent={isTransparent}>
-<div>
-  <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-    <Title>🎬 ScreamFlix</Title>
-  </Link>
-  {location.pathname.startsWith("/movies/") && (
-    <BackButton onClick={handleGoBack}>Movies</BackButton>
-  )}
-</div>
-      <Nav>
-        <Link to="/">Home</Link>
-      </Nav>
+      <div>
+        <Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
+          <Title>🎬 ScreamFlix</Title>
+        </Link>
+        {location.pathname.startsWith('/movies/') && (
+          <BackButton onClick={handleGoBack}>Go back</BackButton>
+        )}
+      </div>
     </StyledHeader>
-  );
-};
+  )
+}
