54a0799b59eec3355c514ebf6a12430da172eaab
alex91-html <alex91-html@users.noreply.github.com>
2025-05-16 12:34:28 +0200

    global style background color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router'
 
 const Card = ({ movie }) => {
   return (
-    <div className="card">
-      <Link to={`/movies/${movie.id}`}>
-        <h2>{movie.title}</h2>
-        <p> Released date: {movie.release_date} </p>
-        <img classImage="card-img" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+    <div className="card" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.poster_path})` }}>
+      <Link to={`/movies/${movie.id}`} className="card-link">
+        <div className="card-overlay">
+          <h2>{movie.title}</h2>
+          <p> Released date: {movie.release_date} </p>
+        </div>
       </Link >
     </div>
   );
