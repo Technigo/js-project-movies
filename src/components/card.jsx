aee97d7ee2355b3c8ed92a73610d03bf92c8d553
Lillebrorgroda <therese.modig@hotmail.com>
2025-05-27 14:15:48 +0200

    fixed styling

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,9 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import styled from 'styled-components'
+import { Link } from "react-router-dom"
 
 const CardContainer = styled.div`
-  position: relative;
-  background-image: ${({ backdrop }) =>
-    `url(https://image.tmdb.org/t/p/w1280${backdrop})`};
+position: relative;
+  background-image: ${({ backdrop }) => `url(https://image.tmdb.org/t/p/w1280${backdrop})`};
   background-size: cover;
   background-position: center;
   height: 600px;
@@ -13,18 +12,21 @@ const CardContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+ 
+
   background-color: #333;
+ 
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s;
   &:hover {
     transform: scale(1);
   }
-`;
+`
 
 const Overlay = styled.div`
   position: absolute;
   inset: 0;
-  background: rgba(0, 0, 0, 0.6);
+  background: rgba(0, 0, 0, 0.6); 
   color: white;
   opacity: 0;
   display: flex;
@@ -38,7 +40,7 @@ const Overlay = styled.div`
     opacity: 1;
   }
 
-  h1 {
+  h2 {
     margin-bottom: 0.5rem;
     font-size: 2rem;
   }
@@ -46,18 +48,20 @@ const Overlay = styled.div`
   p {
     font-size: 1.2rem;
   }
-`;
+`
+
 
 const Card = ({ movie }) => {
   return (
+    //Jasmin added link so that the card on homepage movies links to the moviedetail
     <Link to={`/movies/${movie.id}`}>
       <CardContainer backdrop={movie.poster_path}>
         <Overlay>
-          <h1>{movie.title}</h1>
+          <h2>{movie.title}</h2>
           <p>Released {movie.release_date}</p>
         </Overlay>
       </CardContainer>
     </Link>
-  );
-};
-export default Card;
+  )
+}
+export default Card
