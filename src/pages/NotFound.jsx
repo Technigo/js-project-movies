5ba8e78694007bb176f5967d77265841fa8d83e9
Talo Vargas <govargas@gmail.com>
2025-05-27 14:15:53 +0200

    change error 404 text color to white

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 const NotFound = () => {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-      <h1 className="flex justify-center text-2xl">Error 404: Not Found.</h1>
+      <h1 className="flex justify-center text-white text-2xl">Error 404: Not Found.</h1>
       <Link to="/" className="flex justify-center text-2xl text-blue-700 underline">Back to Home</Link>
     </div>
   )
